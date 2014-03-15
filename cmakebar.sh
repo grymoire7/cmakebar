31261b1758edc08f5fb372dba6913198d6581e95
Tracy Atteberry <tracy@magicbydesign.com>
2014-03-15 00:29:03 -0500

    added usage, options, and logging

--- cmakebar.sh
+++ cmakebar.sh
@@ -8,19 +8,48 @@
 # Usage:
 #
 #     build_technology make 2>&1 | cmakebar.sh
-#     build_technology make 2>&1 | cmakebar.sh --out cmake.log
-#     build_technology make 2>&1 | cmakebar.sh -o
-#     cat cmake.log | cmakebar.sh --replay
-#     cat cmake.log | cmakebar.sh -r
+#     build_technology make 2>&1 | cmakebar.sh -o cmake.log
+#     build_technology make 2>&1 | cmakebar.sh -ro cmake.log
+#     build_technology make 2>&1 | cmakebar.sh -l
+#     build_technology make 2>&1 | cmakebar.sh -rl
+#     cat cmake.log | cmakebar.sh
 #
 # Todo:
-#  [ ] Change timer resolution to remove days
-#  [ ] Change timer resolution to include fractional seconds
+#   [ ] See if there's a faster way than read -r line
+#       Bash version processes a.log in 8s
+#       Go   version processes a.log in 83ms
+#       This could be dwarfed by cmake live times, but still...
 #
 # Author: Tracy Atteberry
 # Date:   Spring 2014
 
 TWIDTH=`tput cols`
+
+usage()
+{
+    cat <<'EOM'
+
+Options: $0 [-h | -o cmake.log | -l | -r ]
+
+    -h        Show this help
+    -r        Remove existing log file before logging
+              Otherwise append if it exists
+    -l        Log to cmake.log
+    -o file   Log to named file
+
+Usage:
+
+    build_technology make 2>&1 | $0
+    build_technology make 2>&1 | $0 -o cmake.log
+    build_technology make 2>&1 | $0 -ro cmake.log
+    build_technology make 2>&1 | $0 -l
+    build_technology make 2>&1 | $0 -rl
+    cat cmake.log | $0
+
+
+EOM
+    exit 1
+}
 
 # Usage: repeat str num
 #    str  is the string to repeat
@@ -56,28 +85,56 @@ highlightTodo()
 #   printf 'Elapsed time: %s\n' $(timer $t)
 #      ===> Elapsed time: 0:01:12
 #
-# Source:
+# Original source:
 # http://www.linuxjournal.com/content/use-date-command-measure-elapsed-time
 #
 # If called with no arguments a new timer is returned.
 # If called with arguments the first is used as a timer
 # value and the elapsed time is returned in the form HH:MM:SS.
 #
+# ---
+# Updated to track nanoseconds and report fractional seconds for
+# times less than a minute.
+#
+# ---
+# Un-updated to not track nanoseconds until thing move fast enough
+# for it matter.
+#
 timer()
 {
     if [[ $# -eq 0 ]]; then
+        # echo $(date '+%s%N')
         echo $(date '+%s')
     else
+        # local  ns_stime=$1
+        # ns_etime=$(date '+%s%N')
+        # if [[ -z "$ns_stime" ]]; then ns_stime=$ns_etime; fi
+        # etime=$(( ns_etime / 1000000000 )) # to seconds
+        # stime=$(( ns_stime / 1000000000 )) # to seconds
+
         local  stime=$1
         etime=$(date '+%s')
-
         if [[ -z "$stime" ]]; then stime=$etime; fi
 
         dt=$((etime - stime))
         ds=$((dt % 60))
         dm=$(((dt / 60) % 60))
         dh=$((dt / 3600))
-        printf '%d:%02d:%02d' $dh $dm $ds
+        if [[ $dh -ne 0 ]]; then
+            printf '%d:%02d:%02d' $dh $dm $ds
+        else
+            if [[ $dm -ne 0 ]]; then
+                printf '%02dm:%02ds' $dm $ds
+            else
+                # ns=$(( ns_etime - ns_stime - (ds*1000000000) ))
+                # ns=$(( ns / 1000000 ))
+                # if [[ $ns -lt 0 ]]; then
+                #     ns=0
+                # fi
+                # printf '%2d.%03ds' $ds $ns
+                printf '%2ds' $ds
+            fi
+        fi
     fi
 }
 
@@ -97,7 +154,8 @@ progress()
 
     percent=$((100 * current / total ))
     printf -v prefix " $percent%%"
-    printf -v postfix "$elapsed"
+    # printf -v postfix "$elapsed"
+    postfix=$elapsed
     bar_size=$(($TWIDTH - ${#prefix} - ${#bar_start} - ${#bar_end} - ${#postfix}))
     amount=$(( bar_size * current / total ))
     remain=$(( bar_size - amount ))
@@ -114,33 +172,81 @@ progress()
 ###############################################################################
 # Main
 #
+logfile=""
+rmlogfile=false
+
+# get options
+while getopts "rhlo:" flag; do
+    case $flag in
+        h)
+            usage
+            ;;
+        r)
+            rmlogfile=true
+            ;;
+        o)
+            logfile=$OPTARG
+            echo "Loging to $logfile."
+            ;;
+        l)
+            logfile="cmake.log"
+            echo "Loging to $logfile."
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+# if logging and file exists then delete it
+if [ ! -z "$logfile" ]; then
+    if [ -e "$logfile" ]; then
+        if $rmlogfile; then
+            rm "$logfile"
+        fi
+    fi
+fi
+
+# # DEBUG
+# if [ -z "$logfile" ]; then
+#     echo "Logging is off."
+# fi
+# exit 1
+
 printf "\n"
 
 isDone=false
 percentPat="^\[ *([0-9]+)%\]"
 failedPat="^Failed Modules"
 t=$(timer)
-# grep -Eo '^\[ *[0-9]+%\]' | grep -Eo '[0-9]+' | while read i
-while read line
+
+while IFS='' read -r line
 do
+    if [ ! -z "$logfile" ]; then
+        echo "$line" >> $logfile
+    fi
+
     if [[ $line =~ $failedPat ]]; then
         printf "\n\n\n"
         isDone=true
     fi
+
     if $isDone; then
         echo $line
         continue
     fi
+
     if [[ ! $line =~ $percentPat ]]; then
         continue
     fi
     i=${BASH_REMATCH[1]}
+
     elapsed=$(timer $t)
     prog=$(progress $i 100 $elapsed)
     printf "%s\r" "$prog"
-    # sleep 0.01
 done
 
-printf "\nDone.\n\n"
+printf "\nDone. $elapsed\n\n"
 
 
